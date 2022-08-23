1f79895243b5fd50e871bfbf47134afa185dcd9f
onesuper <onesuperclark@gmail.com>
2022-08-23 22:00:12 +0800

    pg on conflict do nothing

--- cli/blockchainetl/streaming/postgres_utils.py
+++ cli/blockchainetl/streaming/postgres_utils.py
@@ -6,11 +6,8 @@ def create_insert_statement_for_table(table):
 
     primary_key_fields = [column.name for column in table.columns if column.primary_key]
     if primary_key_fields:
-        insert_stmt = insert_stmt.on_conflict_do_update(
+        insert_stmt = insert_stmt.on_conflict_do_nothing(
             index_elements=primary_key_fields,
-            set_={
-                column.name: insert_stmt.excluded[column.name] for column in table.columns if not column.primary_key
-            }
         )
 
     return insert_stmt
